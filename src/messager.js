6626397d5a7d8f166ca08a5c56332ed11bd347ce
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-28 14:03:32 +0200

    Update the config

--- src/messager.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export function showMessage(messageText) {
-  const messageEl = document.createElement('div');
-  messageEl.textContent = messageText;
-  document.body.appendChild(messageEl);
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,36 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    vendor: './src/vendor.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif|mp3)$/i,
+        test: /\.s[ac]ss$/i,
         use: [
-          {
-            loader: 'file-loader',
-          },
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
         ],
       },
+      {
+        test: /\.(gif|svg|jpg|png)$/,
+        loader: 'file-loader',
+      },
     ],
-    plugins: [
-      new CopyWebpackPlugin([
-        {
-          from: path.resolve(__dirname, 'index.html'),
-          to: path.resolve(__dirname, 'dist'),
-        },
-        {
-          from: path.resolve(__dirname, 'assets', '**', '*'),
-          to: path.resolve(__dirname, 'dist'),
-        },
-      ]),
-      new webpack.DefinePlugin({
-        'typeof CANVAS_RENDERER': JSON.stringify(true),
-        'typeof WEBGL_RENDERER': JSON.stringify(true),
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'production-dependencies',
-        filename: 'production-dependencies.bundle.js',
-      }),
-    ],
-    devServer: {
-      contentBase: path.resolve(__dirname, 'dist'),
-    },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
   },
 };
